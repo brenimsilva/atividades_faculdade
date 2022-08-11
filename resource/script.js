1472af7db59a8051cc66f9677c61e3dbbb829996
brenimsilva <brenimsilva7@gmail.com>
2022-08-11 01:32:06 -0300

    feature: show (A U B U C)

--- resource/script.js
+++ resource/script.js
@@ -1,10 +1,24 @@
 import { setList } from "./sets.js";
 
 const btnConfirm = document.getElementById("btn_show_results");
+const setsListDiv = document.querySelector(".sets_list");
 
 setList.forEach((set) => {
   set.inputBtn.addEventListener("click", () => {
-    set.numList.push(set.numInput.value);
-    set.box.innerText = `{ ${set.numList.join(", ")} }`;
+    if (!set.numInput.value) {
+      throw new Error("Precisa Inserir um número");
+    } else {
+      set.numList.push(set.numInput.value);
+      set.box.innerText = `{ ${set.numList.join(", ")} }`;
+    }
   });
+});
+
+btnConfirm.addEventListener("click", (e) => {
+  e.preventDefault();
+  const allSets = [];
+  setList.forEach((set) => {
+    allSets.push(...set.numList);
+  });
+  setsListDiv.innerText = `{ ${[...new Set(allSets)].join(`, `)} }`;
 });
